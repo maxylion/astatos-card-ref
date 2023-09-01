7d8eebd1134998a4efeb6e6d87636bc81c72e17c
maxylion <theottermax@gmail.com>
2023-09-01 19:28:26 -0300

    (s)

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ setInterval(() => {
 	let deltaMins = 60 - now.getUTCMinutes();
 	let deltaSec = 60 - now.getUTCSeconds();
 	console.log(deltaHours, deltaMins, deltaSec);
-	resetTimerText.textContent = `Daily reset in: ${deltaHours} hours, ${deltaMins} minutes, ${deltaSec} seconds!`
+	resetTimerText.textContent = `Daily reset in: ${deltaHours} hour(s), ${deltaMins} minute(s), ${deltaSec} second(s)!`
 }, 1000)
