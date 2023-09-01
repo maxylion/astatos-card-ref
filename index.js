a42e61a6792f86da9c971d3438e65ebec5073af1
maxylion <theottermax@gmail.com>
2023-09-01 19:50:14 -0300

    montly skin + season reset timer

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
-const resetTimerText = document.getElementById("resettimer");
+const dailyResetTimerText = document.getElementById("dailyResetTimer");
+const monthlySkinResetTimerText = document.getElementById("monthlySkinResetTimer");
+const seasonResetTimerText = document.getElementById("seasonResetTimer");
 
+function daysInMonth(year, month){
+	return new Date(Date.UTC(year, month, 0)).getUTCDate();
+}
 
 setInterval(() => {
 	let now = new Date();
-	console.log(now.toUTCString());
-	let deltaHours = 23 - now.getUTCHours();
-	let deltaMins = 60 - now.getUTCMinutes();
-	let deltaSec = 60 - now.getUTCSeconds();
-	console.log(deltaHours, deltaMins, deltaSec);
-	resetTimerText.textContent = `Daily reset in: ${deltaHours} hour(s), ${deltaMins} minute(s), ${deltaSec} second(s)!`
+	const monthDaysSeason_1 = daysInMonth(now.getUTCFullYear(), now.getUTCMonth());
+	const monthDaysSeason_2 = daysInMonth(now.getUTCFullYear(), now.getUTCMonth()+1);
+	const monthDaysSeasonTotal = (monthDaysSeason_1 + monthDaysSeason_2) - monthDaysSeason_1 -1;
+	const monthDaysSkin = daysInMonth(now.getUTCFullYear(), now.getUTCMonth());
+	const deltaDays = ((monthDaysSkin - 1) - now.getUTCDate());
+	const deltaHours = 23 - now.getUTCHours();
+	const deltaMins = 59 - now.getUTCMinutes();
+	const deltaSec = 59 - now.getUTCSeconds();
+	dailyResetTimerText.textContent = `Daily reset in: ${deltaHours} hour(s), ${deltaMins} minute(s), ${deltaSec} second(s)!`
+	monthlySkinResetTimerText.textContent = `Montly skin reset in: ${deltaDays} day(s)!`
+	seasonResetTimerText.textContent = `Season reset in: ${monthDaysSeasonTotal} day(s)`
 }, 1000)
